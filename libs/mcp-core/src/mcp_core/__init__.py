8742d990202254bd451687f62063655ad1b72d92
JaehoonSong12 <manual20151276@gmail.com>
2026-06-10 19:27:16 -0400

    init

---
+++ libs/mcp-core/src/mcp_core/__init__.py
@@ -0,0 +1,7 @@
+from .calculator import multiply, add_numbers, subtract, divide
+from .feed import fcc_news_search, fcc_youtube_search, fcc_secret_message
+
+__all__ = [
+    "multiply", "add_numbers", "subtract", "divide",
+    "fcc_news_search", "fcc_youtube_search", "fcc_secret_message"
+]
